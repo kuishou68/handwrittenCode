4136e10f3a3e250a092b01e5dd7d5aefec7d700a
kuishou68 <1569339843@qq.com>
2024-05-08 21:51:01 +0800

    二叉树的最大深度

--- index.js
+++ index.js
@@ -1181,3 +1181,7 @@ var minDepth = function (root) {
     if(!root.left && root.right) return 1+right;
     return 1+Math.min(left, right);
 }
+
+/**
+ * -------------------------------------------二叉树的最大深度-------------------------------------------
+ */
