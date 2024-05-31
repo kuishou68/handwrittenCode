ec582fcc1a872a5adb1c83fd54004f2e1e77bdcf
kuishou58 <1569339843@qq.com>
2024-05-31 11:18:47 +0800

    update

--- index.js
+++ index.js
@@ -1063,28 +1063,29 @@ var twoSum = function(nums, target) {
 };
 
 
-/*
+/**
  * -------------------------------------------三数之和-------------------------------------------
  *  给你一个整数数组 nums ，判断是否存在三元组 [nums[i], nums[j], nums[k]] 满足 i != j、i != k 且 j != k ，同时还满足 nums[i] + nums[j] + nums[k] == 0 。请
+ *
  *  示例 1：
-    输入：nums = [-1,0,1,2,-1,-4]
-    输出：[[-1,-1,2],[-1,0,1]]
-    解释：
-    nums[0] + nums[1] + nums[2] = (-1) + 0 + 1 = 0 。
-    nums[1] + nums[2] + nums[4] = 0 + 1 + (-1) = 0 。
-    nums[0] + nums[3] + nums[4] = (-1) + 2 + (-1) = 0 。
-    不同的三元组是 [-1,0,1] 和 [-1,-1,2] 。
-    注意，输出的顺序和三元组的顺序并不重要。
-
-    示例 2：
-    输入：nums = [0,1,1]
-    输出：[]
-    解释：唯一可能的三元组和不为 0 。
-
-    示例 3：
-    输入：nums = [0,0,0]
-    输出：[[0,0,0]]
-    解释：唯一可能的三元组和为 0 。
+ *     输入：nums = [-1,0,1,2,-1,-4]
+ *     输出：[[-1,-1,2],[-1,0,1]]
+ *     解释：
+ *     nums[0] + nums[1] + nums[2] = (-1) + 0 + 1 = 0 。
+ *     nums[1] + nums[2] + nums[4] = 0 + 1 + (-1) = 0 。
+ *     nums[0] + nums[3] + nums[4] = (-1) + 2 + (-1) = 0 。
+ *     不同的三元组是 [-1,0,1] 和 [-1,-1,2] 。
+ *     注意，输出的顺序和三元组的顺序并不重要。
+ *
+ *  示例 2：
+ *     输入：nums = [0,1,1]
+ *     输出：[]
+ *     解释：唯一可能的三元组和不为 0 。
+ *
+ *  示例 3：
+ *     输入：nums = [0,0,0]
+ *     输出：[[0,0,0]]
+ *     解释：唯一可能的三元组和为 0
  */
 var threeSum = function(nums) {
     let ans = [];
@@ -1115,36 +1116,49 @@ var threeSum = function(nums) {
 };
 
 var threeSum = function(nums){
-    let res = [];
+    let ans = [];
     let len = nums.length;
-    if(len < 3) return res;
+    if(len < 3) return ans;
     nums.sort((a,b) => a-b);
-    for(let i = 0; i < len; i++){
+    for(let i = 0; i<len; i++){
         if(nums[i] > 0) break;
-        if(i > 0 && nums[i] === nums[i-1]) continue;
-        let l = i+1;
-        let r = len-1;
-        while(l<r){
+        if(i>0 && nums[i] === nums[i-1]) continue;
+        let l = i + 1;
+        let r = len - 1;
+        while(l<r) {
             let sum = nums[i] + nums[l] + nums[r];
             if(sum === 0){
-                res.push([nums[i], nums[l], nums[r]]);
-                while(l<r && nums[l] === nums[l+1]) l++;
-                while(l<r && nums[r] === nums[r-1]) r--;
+                ans.push([nums[i], nums[l], nums[r]]);
+                while(l < r && nums[l] === nums[l+1]) l++;
+                while(l < r && nums[r] === nums[r-1]) r--;
                 l++;
                 r--;
-            } else if(nums < 0){
+            } else if(sum < 0){
                 l++;
             } else if(sum > 0){
                 r--;
             }
         }
     }
-    return res;
+    return ans;
 }
-
 /**
  * -------------------------------------------四数之和-------------------------------------------
+ * 给你一个由 n 个整数组成的数组 nums ，和一个目标值 target 。请你找出并返回满足下述全部条件且不重复的四元组 [nums[a], nums[b], nums[c], nums[d]] （若两个四元组元素一一对应，则认为两个四元组重复）：
  *
+ * 0 <= a, b, c, d < n
+ * a、b、c 和 d 互不相同
+ * nums[a] + nums[b] + nums[c] + nums[d] == target
+ * 你可以按 任意顺序 返回答案 。
+ *
+ * 示例 1：
+ *
+ * 输入：nums = [1,0,-1,0,-2,2], target = 0
+ * 输出：[[-2,-1,1,2],[-2,0,0,2],[-1,0,0,1]]
+ * 示例 2：
+ *
+ * 输入：nums = [2,2,2,2,2], target = 8
+ * 输出：[[2,2,2,2]]
  */
 /**
  * @param {number[]} nums
@@ -1181,6 +1195,42 @@ var  fourSum = function(nums, target){
                 } else if(sum > target){
                     r--;
                 }
+            }
+        }
+    }
+    return res;
+}
+
+var fourSum = function (nums, target){
+    let res = [];
+    let len = nums.length;
+    if(len < 4) return res;
+    nums.sort((a,b) => a-b);
+    for(let i = 0; i < len - 3; i++){
+        if(i === 0 || nums[i] > nums[i-1]){
+            let l = i+1;
+            let j = l+1;
+            while(l < len - 2){
+                let r = len - 1;
+                if(l === i+1 || nums[l] > nums[l-1]){
+                    while(i < len - 1 && j < r){
+                        const sum = nums[i] + nums[l] + nums[j] + nums[r];
+                        const ans = [nums[i], nums[l], nums[j], nums[r]];
+                        if(sum === target){
+                            res.push(ans);
+                            j++;
+                            r--;
+                            while(nums[j] === nums[j-1]) j++;
+                            while(nums[r] === nums[r+1]) r--;
+                        } else if(sum < target){
+                            j++;
+                        } else if(sum > target){
+                            r--;
+                        }
+                    }
+                }
+                l++;
+                j=l+1;
             }
         }
     }
