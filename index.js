a9a3df95ac2968b07495b3076c0e4f16aefdad12
kuishou68 <1569339843@qq.com>
2024-11-22 18:00:53 +0800

    快乐数

--- index.js
+++ index.js
@@ -2041,3 +2041,66 @@ const majorityElement = nums => {
     return majority;
 };
 
+const majorityElement = nums => {
+    let len = nums.length;
+    if(!len){ return len; }
+    for(let i = 0; i < len; i++){
+        let count = 0;
+        for(let j = i; j < len; j++){
+            if(nums[i] === nums[j]){
+                count++;
+            }
+        }
+        if(count > len/2){
+            return num[i];
+        }
+    }
+}
+
+/**
+ * -------------------------------------------快乐数-------------------------------------------
+ * 编写一个算法来判断一个数 n 是不是快乐数。
+ *
+ * 「快乐数」 定义为：
+ *  对于一个正整数，每一次将该数替换为它每个位置上的数字的平方和。
+ *  然后重复这个过程直到这个数变为 1，也可能是 无限循环 但始终变不到 1。
+ *  如果这个过程 结果为 1，那么这个数就是快乐数。
+ *
+ *
+ *  如果 n 是 快乐数 就返回 true ；不是，则返回 false 。
+ *
+ * 示例 1：
+ *  输入：n = 19
+ *  输出：true
+ *  解释：
+ *  1² + 9² = 82
+ *  8² + 2² = 68
+ *  6² + 8² = 100
+ *  1² + 0² + 0² = 1
+ *
+ *
+ * 示例 2：
+ *  输入：n = 2
+ *  输出：false
+ *
+ **/
+
+var isHappy = function(n) {
+    if (n <= 4) {
+        return n == 1;
+    } else {
+        console.log(n)
+        let sum = 0;
+        String(n).split('').map(item => {
+            sum += Number(item) * Number(item);
+        })
+        return isHappy(sum);
+    }
+};
+
+
+
+
+
+
+
