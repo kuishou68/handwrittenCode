b80bf49be001778b3d41ccffc7814461fc0a7643
kuishou68 <1569339843@qq.com>
2024-11-26 16:25:45 +0800

    同构字符串

--- index.js
+++ index.js
@@ -2184,12 +2184,6 @@ var removeElements = function(head, val) {
  *   输入：s = "bbbaaaba", t = "aaabbbba"
  *   输出：false
  *
- *  提示：
- *  1 <= s.length <= 5 * 10⁴
- *  t.length == s.length
- *  s 和 t 由任意有效的 ASCII 字符组成
- *
- *
  */
 /**
  * @param {string} s
@@ -2214,5 +2208,43 @@ var isIsomorphic = function(s, t) {
     return true;
 };
 
-//leetcode submit region end(Prohibit modification and deletion)
+
+/**
+ * -------------------------------------------存在重复元素-------------------------------------------
+ *
+ * 给你一个整数数组 nums 。如果任一值在数组中出现 至少两次 ，返回 true ；如果数组中每个元素互不相同，返回 false 。
+ *
+ *  示例 1：
+ *  输入：nums = [1,2,3,1]
+ *  输出：true
+ *  解释：
+ *  元素 1 在下标 0 和 3 出现。
+ *
+ *  示例 2：
+ *  输入：nums = [1,2,3,4]
+ *  输出：false
+ *  解释：
+ *  所有元素都不同。
+ *
+ *  示例 3：
+ *  输入：nums = [1,1,1,3,3,4,3,2,4,2]
+ *  输出：true
+ *
+ *  提示：
+ *  1 <= nums.length <= 10⁵
+ *  -10⁹ <= nums[i] <= 10⁹
+ *
+ *
+ * */
+var containsDuplicate = function (nums) {
+  let len = nums.length;
+  if(len < 2) { return false; }
+  let map = new Map();
+  for(let i = 0; i < len; i++){
+      if(map.has(nums[i])){ return true;  }
+      map.set(nums[i], true)
+      if(i+1 === len){ return false; }
+  }
+};
+
 
