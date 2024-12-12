a8b1cb437a39a44e7f5999bac71b0a65af9f97a7
kuishou68 <1569339843@qq.com>
2024-12-12 16:02:06 +0800

    单词规则

--- index.js
+++ index.js
@@ -2676,4 +2676,52 @@ var moveZeroes = function(nums) {
     }
 };
 
+/**
+ * ------------------------------------------- 单词规则 -------------------------------------------
+ * 给定一种规律 pattern 和一个字符串 s ，判断 s 是否遵循相同的规律。 
+ * 
+ * 这里的 遵循 指完全匹配，例如， pattern 里的每个字母和字符串 s 中的每个非空单词之间存在着双向连接的对应规律。 
+ * 
+ * 
+ * 示例1: 
+ * 输入: pattern = "abba", s = "dog cat cat dog"
+ * 输出: true 
+ * 
+ * 示例 2: 
+ * 输入:pattern = "abba", s = "dog cat cat fish"
+ * 输出: false 
+ * 
+ * 示例 3: 
+ * 输入: pattern = "aaaa", s = "dog cat cat dog"
+ * 输出: false 
+ * 
+ *  提示: 
+ *  1 <= pattern.length <= 300 
+ *  pattern 只包含小写英文字母 
+ *  1 <= s.length <= 3000 
+ *  s 只包含小写英文字母和 ' ' 
+ *  s 不包含 任何前导或尾随对空格 
+ *  s 中每个单词都被 单个空格 分隔 
+ * 
+ */
 
+/**
+ * @param {string} pattern
+ * @param {string} s
+ * @return {boolean}
+ */
+var wordPattern = function(pattern, s) {
+    let sArr = s.split(' ');
+    if(pattern.length !== sArr.length){ return false; }
+    let pMap = new Map();
+    let sMap = new Map();
+    for(let i = 0; i < sArr.length; i++){
+        const x = sArr[i], y = pattern[i];
+        if(pMap.has(x) && pMap.get(x) !== y || sMap.has(y) && sMap.get(y) !== x){
+            return false;
+        }
+        pMap.set(x, y);
+        sMap.set(y, x);
+    }
+    return true;
+};
