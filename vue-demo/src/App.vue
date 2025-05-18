809f5eb8661e34053209ae2c4b6658c856c64f38
kuishou68 <1569339843@qq.com>
2025-05-18 09:59:55 +0800

    update

---
+++ vue-demo/src/App.vue
@@ -0,0 +1,21 @@
+<script setup>
+import { watch, reactive } from 'vue';
+
+const state = reactive({
+	a: {},
+	c: 4
+})
+
+watch(() => {
+		console.log(state);
+		return state.a;
+	},
+	(val) => {
+		console.log('2');
+	}
+)
+
+setTimeout(() => {
+	state.c++;
+})
+</script>
